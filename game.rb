9c8795565ca69bfb119c8664411e5c06b39048b2
Johan Nygård <johan@Johan-desktop>
2012-02-25 17:41:21 +0100

    added some factory functions.

--- game.rb
+++ game.rb
@@ -19,7 +19,9 @@ class Game
     @balls = []
     @blocks = []
     create_court
-    100.times {@balls << (Ball.new 250+rand(500), 250+rand(500), 3 + rand(7), rand(100)-100,rand(100)-100) }
+    @blocks += create_breakables [@screen.width/2,@screen.width/2]
+    
+    50.times {@balls << (Ball.new 250+rand(500), 250+rand(500), 3 + rand(7), rand(100)-100,rand(100)-100) }
     @collisiondetector = CollisionSupervisor.new @balls, @blocks, @background
   end
 
@@ -58,11 +60,48 @@ class Game
   end
   
   def create_court
-    for n in 1..6 do
-      @blocks << (Block.new (@screen.width/2.5)*sin(2*PI*n/6)+@screen.width/2,(@screen.width/2.5)*cos(2*PI*n/6)+@screen.width/2, @screen.width/2.11, 20, 60*(n-1) + 60)
-    end
+    @blocks += hexagon_factory([@screen.width/2,@screen.width/2],Block,@screen.width,20)
   end
-
+  #move this? this is only a demo.version.
+  def create_breakables centre
+    blocks=[]
+    for n in 1..5 do
+      blocks += hexsymetric_factoy([0,45*n],centre,Block,50,5)
+      blocks += hexsymetric_factoy([39,22.5+45*n],centre,Block,50,5)
+    end
+    for n in 2..5 do
+      blocks += hexsymetric_factoy([-39,22.5+45*n],centre,Block,50,5)
+    end
+    for n in 3..5 do
+      blocks += hexsymetric_factoy([78,45*n],centre,Block,50,5)
+    end
+    for n in 4..5 do
+      blocks += hexsymetric_factoy([-78,45*n],centre,Block,50,5)
+    end
+    return blocks
+  end
+  #move this some where else?
+  def hexagon_factory position, blocktype, width, thickness
+    blocks=[]
+    for n in 1..6 do
+      blocks << blocktype.new((width/2.5)*sin(2*PI*n/6)+position[0],(width/2.5)*cos(2*PI*n/6)+position[1], width/2.11, thickness, 60*(n-1) + 60)
+    end
+    return blocks
+  end
+  #move this somwhere else?
+  def hexsymetric_factoy position, centre, blocktype, width, thickness
+    blocks=[]
+    for n in 1..6 do 
+      xpos=position[0]*cos(2*PI*n/6) - position[1]*sin(2*PI*n/6)+centre[0]
+      ypos=position[0]*sin(2*PI*n/6) + position[1]*cos(2*PI*n/6)+centre[1]
+      blocks += hexagon_factory([xpos,ypos],blocktype,width,thickness)
+      for i in 0..5 do #TODO remove only to make better visuals for now
+        blocks[(n-1)*6+i].color=[rand(255),rand(255),rand(255)]
+        puts blocks[(n-1)*6+i].color
+      end
+    end
+    return blocks
+  end
 end
 g = Game.new
 g.run!
